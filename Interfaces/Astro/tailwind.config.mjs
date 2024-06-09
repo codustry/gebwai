87298345b12f39de6d1c45abadb901f05b1a52b8
Nutchanon Ninyawee <nutchanon@codustry.com>
2024-06-09 07:08:07 +0700

    feat(Astro): :sparkles: complete frontpage

--- Interfaces/Astro/tailwind.config.mjs
+++ Interfaces/Astro/tailwind.config.mjs
@@ -1,11 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 
 import daisyui from "daisyui"
+import defaultTheme from 'tailwindcss/defaultTheme'
+
+// https://designsystem.line.me/LDSM/foundation/color/line-color-guide-ex-en
 
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				LINE: "#06C755",
+			},
+			fontFamily: {
+				sans: ['Prompt', ...defaultTheme.fontFamily.sans],
+			},
+		},
 	},
 	plugins: [
 		daisyui,
